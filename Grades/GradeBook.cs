adf5ae12788b80ec197a68a9e4e8eca1b99240b0
Marcus Bernhard <Marcus.Bernhard@outlook.com>
2018-10-14 12:00:09 +0200

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+           grades  = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                sum += grade;
+            }
+
+            return stats;
+        }
+
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+
+    }
+}
